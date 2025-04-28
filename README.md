d6582cfcb1452085c54614985817c75fd6adabfa
Andy Hopper <andy.hopper@shippo.com>
2025-04-28 18:07:09 -0400

    Added sample code.

---
+++ README.md
@@ -0,0 +1,25 @@
+# <img src="https://docs.goshippo.com/images/Logo.png" width="30" alt="Shippo logo"> Shippo Javascript SDK demo
+
+* Collect shipping rates offered by the carriers configured in your Shippo account
+* Use a selected rate to request a label
+* Download the label for use in shipping your package
+
+## Running the code
+
+Before you run this code, you will need to have performed the below steps:
+1. Install a recent version of Python 3 (this sample was written using 3.10)
+2. [Create a Shippo Account](https://apps.goshippo.com/join)
+3. [Generate a Shippo API Token](https://support.goshippo.com/hc/en-us/articles/360026412791-Managing-Your-API-Tokens-in-Shippo#:~:text=Generate%20a%20Token,-To%20generate%20a&text=To%20generate%20a%20Test%20Token,and%20purchase%20test%20shipping%20labels.). Since this is a sample app, it is recommended that you generate a test token rather than a production (i.e., paid) token.
+4. Update the code in shippo-python-sdk-demo.py to use your token
+``` python
+  # Put your real token below...
+  sdk = shippo.Shippo(api_key_header="shippo_test_0123456789abcdef0123456789abcdef01234567")
+```
+5. Install pre-requisites
+```shell
+npm install
+```
+6. Run the sample!
+```shell
+npm run
+```
---
+++ index.mjs
@@ -0,0 +1,80 @@
+import { Shippo } from "shippo";
+import os from 'os';
+import fs from 'fs';
+import path from 'path';
+import https from 'https';
+
+const sdk = new Shippo("<YOUR API TOKEN>");
+
+const shipment = await sdk.shipments.create({
+  "addressFrom": {
+      "name": "Shawn Ippotle",
+      "street1": "215 Clayton St.",
+      "city": "San Francisco",
+      "state": "CA",
+      "zip": "94117",
+      "country": "US",
+      "phone": "+1 555 341 9393",
+      "email": "test@gmail.com"
+  },
+  "addressTo": {
+      "name": "Mr. Hippo",
+      "street1": "1092 Indian Summer Ct",
+      "city": "San Jose",
+      "state": "CA",
+      "zip": "95122",
+      "country": "US",
+      "phone": "+1 555 341 9393",
+      "email": "test@gmail.com"
+  },
+  "parcels": [{
+      "length": "15",
+      "width": "15",
+      "height": "15",
+      "distanceUnit": "cm",
+      "weight": ".2",
+      "massUnit": "kg"
+  }]
+});
+
+console.log (`Shipment ID ${shipment.objectId}`);
+
+// We didn't specify 'async' above, so rates will be returned in the response
+shipment.rates.forEach(rate => {
+  console.log(`${rate.objectId} ${rate.provider} ${rate.servicelevel.name}, Arrives in ${rate.estimatedDays} days: ${rate.amountLocal}${rate.currencyLocal}`)
+});
+
+// Get the lowest rate
+const lowestRate = shipment.rates.sort((a, b) => parseFloat(a.amountLocal) - parseFloat(b.amountLocal))[0];
+
+let transaction = await sdk.transactions.create({
+    labelFileType: "PDF_4x6",
+    metadata: "Order ID #12345",
+    rate: lowestRate.objectId
+  });
+
+console.log(`Transaction ID ${transaction.objectId}`);
+
+while (transaction.status === "QUEUED") {
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  transaction = await sdk.transactions.get(transaction.objectId);
+}
+
+if (transaction.status === "SUCCESS") {
+  console.log(`Label URL: ${transaction.labelUrl}`);
+  console.log(`Tracking URL: ${transaction.trackingUrlProvider}`);
+  const downloadsFolder = path.join(os.homedir(), 'Downloads');
+  const filePath = path.join(downloadsFolder, `${transaction.objectId}.pdf`);
+
+  const file = fs.createWriteStream(filePath);
+  await https.get(transaction.labelUrl, (response) => {
+      response.pipe(file);
+      file.on('finish', () => {
+          file.close();
+          console.log(`Label downloaded to ${filePath}`);
+      });
+  }).on('error', (err) => {
+      fs.unlink(filePath, () => {});
+      console.error(`Error downloading label: ${err.message}`);
+  });
+}
